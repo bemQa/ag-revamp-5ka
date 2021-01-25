7dce4365d706bb81b4e76ceaee32f74a3cc7af88
Alexander K <niktonezapomnil@gmail.com>
2021-01-25 20:28:28 +0500

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -195,6 +195,7 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        swipe: false
     });
 });
